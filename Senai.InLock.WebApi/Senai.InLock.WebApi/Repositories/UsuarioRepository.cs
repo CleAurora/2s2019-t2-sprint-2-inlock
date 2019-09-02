deaca944026c94a846bfa75a50f52408067ef19a
Cleís Aurora Silveira Pereira <34132336883@info.local>
2019-09-02 11:28:15 -0300

    Adiciona login

--- Senai.InLock.WebApi/Senai.InLock.WebApi/Repositories/UsuarioRepository.cs
+++ Senai.InLock.WebApi/Senai.InLock.WebApi/Repositories/UsuarioRepository.cs
@@ -1,13 +1,26 @@
 ﻿using Senai.InLock.WebApi.Domains;
-using System;
+using Senai.InLock.WebApi.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Senai.InLock.WebApi.Repositories
 {
     public class UsuarioRepository
     {
+        public Usuarios BuscarPorEmailESenha(LoginViewModel login)
+        {
+            using (InLockContext ctx = new InLockContext())
+            {
+                Usuarios UsuarioBuscado = ctx.Usuarios.FirstOrDefault(x => x.Email == login.Email && x.Senha == login.Senha);
+                if (UsuarioBuscado == null)
+                {
+                    return null;
+                }
+                return UsuarioBuscado;
+            }
+        }
+
+
         public List<Usuarios> Listar()
         {
             using (InLockContext context = new InLockContext())
